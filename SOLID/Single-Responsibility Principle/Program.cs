e53e9d56dea3bf0b75ee48ee48ee2115ceefc8d4
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-10-16 18:29:56 +0300

    Single Responsibility Principle

--- SOLID/Single-Responsibility Principle/Program.cs
+++ SOLID/Single-Responsibility Principle/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿CustomerManager customerManager = new(new IkinciMevzuat());
+customerManager.Add();
+customerManager.ReadLine();
+
+internal class CustomerManager
+{
+    private IMevzuat _mevzuat;
+
+    public CustomerManager(IMevzuat mevzuat)
+    {
+        _mevzuat = mevzuat;
+    }
+
+    public void Add()
+    {
+    }
+
+    public void ReadLine()
+    {
+        _mevzuat.IslemYap();
+    }
+}
+
+interface IMevzuat
+{
+    void IslemYap();
+}
+
+class BirinciMevzuat : IMevzuat
+{
+    public void IslemYap()
+    {
+        Console.WriteLine("Birinci Mevzuata Göre Eklendi.");
+    }
+}
+class IkinciMevzuat : IMevzuat
+{
+    public void IslemYap()
+    {
+        Console.WriteLine("İkinci Mevzuata Göre Eklendi.");
+    }
+}
